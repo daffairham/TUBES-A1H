93c3532aaa79b8740373a48017827c3ff28b5023
daffairham <dafirham@gmail.com>
2022-11-02 15:18:17 +0700

    intent dari home ke buat pertemuan

---
+++ app/src/main/java/com/example/tubes_a1h/ActivityAppointment.java
@@ -0,0 +1,18 @@
+package com.example.tubes_a1h;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.BaseMenuPresenter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityAppointment extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buat_pertemuan);
+    }
+}
--- app/src/main/java/com/example/tubes_a1h/MainActivity.java
+++ app/src/main/java/com/example/tubes_a1h/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.tubes_a1h;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private Button btnStartActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.btnStartActivity = findViewById(R.id.btn_start);
+        btnStartActivity.setOnClickListener(this);
+    }
+        @Override
+    public void onClick(View view) {
+        if(view.getId()==btnStartActivity.getId()){
+            Intent intent = new Intent(this, ActivityAppointment.class);
+            startActivity(intent);
+        }
     }
 }
