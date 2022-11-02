3ae8b6ac2a1843112ec4ce93937ddf0bb873d929
daffairham <dafirham@gmail.com>
2022-11-02 14:48:50 +0700

    test

--- app/src/main/java/com/example/tubes_a1h/MainActivity.java
+++ app/src/main/java/com/example/tubes_a1h/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.tubes_a1h;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner spinner;
+    String[] dokter = {"Andi", "Budi", "Caca"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.buat_pertemuan);
+        spinner = findViewById(R.id.spinner_dokter);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, dokter);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String value = parent.getItemAtPosition(pos).toString();
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
